66dd2f0aa2b65c9c2d37fc2bb76f9a154fd7c63e
justrandomwords <134196541+justrandomwords@users.noreply.github.com>
2023-05-21 12:33:51 +0300

    Add project files.

---
+++ PressureVisualisation/PressureVisualisation.xaml.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Globalization;
+
+namespace PressureVisualisation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void InitializeSerialPort(string portName, int baudRate)
+        {
+            serialPort = new SerialPort(portName, baudRate);
+
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var item in SerialPort.GetPortNames())
+                NameSerialport.Items.Add(item);
+
+            NameSerialport.SelectedIndex = 0;
+
+            NameSerialport.DropDownOpened += NameSerialport_DropDownOpened;
+            NameSerialport.DropDownClosed += NameSerialport_DropDownClosed;
+
+            try { InitializeSerialPort(NameSerialport.Text, int.Parse(ValueBaudrate.Text)); }
+            catch (Exception) 
+            { NotFoundPorts(); }
+            
+
+
+            ValueBaudrate.TextChanged += NameSerialport_TextChanged;
+        }
+
+
+        SerialPort serialPort;
+        string selectedPort;
+
+        bool isLightTheme = true;
+        bool isNewton = true;
+
+
+        private void ChangeUnits(string value)
+        {
+            isNewton = isNewton ? false : true;
+
+            StringBuilder rezult = new StringBuilder();
+            string[] splitValue = value.Split();
+
+            try
+            {
+                rezult.Append(Math.Round(double.Parse(splitValue[0]) / 9.8, 2).ToString());
+            }
+            catch (Exception)
+            {
+                rezult.Append(splitValue[0]);
+            }
+
+            if (isNewton)
+                rezult.Append(" N");
+            else
+                rezult.Append(" kg");
+
+            outputLabel.Content = rezult;
+        }
+
+        private void NotFoundPorts()
+        {
+            NameSerialport.Items.Insert(0, "not found");
+            NameSerialport.SelectedIndex = 0;
+            NameSerialport.IsTextSearchEnabled = false;
+        }
+
+        private void SetDefaultOutputLabel(string value)
+        {
+            if (isNewton)
+                outputLabel.Content = $"{value} N";
+            else
+                outputLabel.Content = $"{value} kg";
+        }
+
+        private void OpenPort(SerialPort port)
+        {
+            startStopButton.Content = "Stop";
+
+            SetDefaultOutputLabel("0.00");
+            outputLabel.Foreground = (SolidColorBrush)Application.Current.Resources["StartOutputText"];
+            outputLabel.BorderBrush = (SolidColorBrush)Application.Current.Resources["StartOutputText"];
+            port.Open();
+        }
+
+        private void ClosePort(SerialPort port)
+        {
+            startStopButton.Content = "Start";
+
+            SetDefaultOutputLabel("XX");
+            outputLabel.Foreground = (SolidColorBrush)Application.Current.Resources["MainText"];
+            outputLabel.BorderBrush = (SolidColorBrush)Application.Current.Resources["MainText"];
+            port.Close();
+        }
+
+        private void NameSerialport_DropDownOpened(object sender, EventArgs e)
+        {
+            if(NameSerialport.Text != "" && NameSerialport.Text != "not found")
+                selectedPort = NameSerialport.Text;
+
+            NameSerialport.Items.Clear();
+
+            foreach (var item in SerialPort.GetPortNames())
+                NameSerialport.Items.Add(item);
+        }
+
+        private void NameSerialport_DropDownClosed(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                ClosePort(serialPort);
+
+            if (NameSerialport.Items.Count == 0)
+            {
+                NotFoundPorts();
+            }
+            else
+            {
+                if (NameSerialport.SelectedItem == null)
+                    NameSerialport.SelectedIndex = int.Parse(selectedPort[3].ToString()) - 1;
+
+                InitializeSerialPort(NameSerialport.Text, int.Parse(ValueBaudrate.Text));
+            }
+        }
+
+        private void NameSerialport_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                ClosePort(serialPort);
+
+            InitializeSerialPort(NameSerialport.Text, int.Parse(ValueBaudrate.Text));
+        }
+
+
+        public SolidColorBrush CalculateIntermediateColor(Color color1, Color color2, double number)
+        {
+            number /= 255;
+
+            byte red = (byte)(color1.R + (color2.R - color1.R) * number);
+            byte green = (byte)(color1.G + (color2.G - color1.G) * number);
+            byte blue = (byte)(color1.B + (color2.B - color1.B) * number);
+            byte alpha = (byte)(color1.A + (color2.A - color1.A) * number);
+
+            return new SolidColorBrush(Color.FromArgb(alpha, red, green, blue));
+        }
+
+        private SolidColorBrush ChangeColor(double number, int max, bool isGlow = false)
+        {
+            number /= max / 255.0;
+            number = number >= 255 ? 254 : number;
+
+            SolidColorBrush currentColor;
+
+            if (!isGlow)
+                currentColor = CalculateIntermediateColor(
+                ((SolidColorBrush)Application.Current.Resources["StartOutputText"]).Color,
+                ((SolidColorBrush)Application.Current.Resources["EndOutputText"]).Color,
+                number);
+            else
+                currentColor = CalculateIntermediateColor(
+                ((SolidColorBrush)Application.Current.Resources["StartOutputText"]).Color,
+                ((SolidColorBrush)Application.Current.Resources["EndOutputTextGlow"]).Color,
+                number);
+
+
+            return currentColor;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dirtyData = serialPort.ReadExisting();
+            string data = dirtyData.Split('\n')[0];
+
+            Dispatcher.Invoke(() => 
+            {
+                if (isNewton)
+                    outputLabel.Content = Math.Round(double.Parse(data.TrimEnd('\n'), CultureInfo.InvariantCulture) * 9.8, 2).ToString("F2").Replace(',', '.') + " N";
+                else
+                    outputLabel.Content = data.TrimEnd('\n') + " kg";
+
+                outputLabel.Foreground = ChangeColor(double.Parse(data, CultureInfo.InvariantCulture), 5);
+                outputLabel.BorderBrush = ChangeColor(double.Parse(data, CultureInfo.InvariantCulture), 5, true);
+            });
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+                ClosePort(serialPort);
+            else
+                OpenPort(serialPort);
+        }
+
+        private void WindowControl_Highlighte(object sender, MouseEventArgs e)
+        {
+            Brush defaultButons = (SolidColorBrush)Application.Current.Resources["MainText"];
+            Brush exitButton = (SolidColorBrush)Application.Current.Resources["HighlightedExitButton"];
+            Brush othersButton = (SolidColorBrush)Application.Current.Resources["HighlightedOthersButton"];
+
+
+            Rectangle currentRectangle;
+
+            switch (((Button)sender).Name)
+            {
+                case "CollapseButton":
+                    currentRectangle = (Rectangle)FindName("CollapseButtonLabel");
+                    currentRectangle.Fill = currentRectangle.Fill == defaultButons ?
+                            othersButton : defaultButons;
+                    break;
+                case "ExpandButton":
+                    currentRectangle = (Rectangle)FindName("ExpandButtonLabel");
+                    currentRectangle.Stroke = currentRectangle.Stroke == defaultButons ?
+                            othersButton : defaultButons;
+                    break;
+                case "ExitButton":
+                    ExitButtonLabel.Fill = ExitButtonLabel.Fill == defaultButons ?
+                            exitButton : defaultButons;
+                    break;
+            }
+        }
+
+        private void WindowControl_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "CollapseButton":
+                    this.WindowState = WindowState.Minimized;
+                    break;
+                case "ExpandButton":
+                    this.WindowState = this.WindowState == WindowState.Maximized ?
+                        WindowState.Normal : WindowState.Maximized;
+                    break;
+                case "ExitButton":
+                    this.Close();
+                    break;
+                default:
+                    throw new Exception("WindowControlsClick run to default");
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void ChangeTheme(string url)
+        {
+            ResourceDictionary newTheme = new ResourceDictionary();
+            newTheme.Source = new Uri(url, UriKind.Relative);
+            Application.Current.Resources.MergedDictionaries[0] = newTheme;
+
+            try 
+            {
+                int value = int.Parse(outputLabel.Content.ToString().Split(' ')[0]);
+
+                outputLabel.Foreground = ChangeColor(value, 1024); 
+                outputLabel.BorderBrush = ChangeColor(value, 1024, true);
+            }
+            catch (Exception) 
+            { 
+                outputLabel.Foreground = (SolidColorBrush)Application.Current.Resources["MainText"];
+                outputLabel.BorderBrush = (SolidColorBrush)Application.Current.Resources["MainText"];
+            }
+
+            CollapseButtonLabel.Fill = (SolidColorBrush)Application.Current.Resources["MainText"];
+            ExitButtonLabel.Fill = (SolidColorBrush)Application.Current.Resources["MainText"];
+
+            isLightTheme = !isLightTheme;
+        }
+
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            if (isLightTheme)
+                ChangeTheme("Themes/MainDark.xaml");
+            else
+                ChangeTheme("Themes/MainLight.xaml");
+        }
+
+        private void ChangeUnits_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            ChangeUnits(outputLabel.Content.ToString());
+        }
+    }
+}
---
+++ WokwiToComPort/WokwiToComPort.xaml.cs
@@ -0,0 +1,316 @@
+﻿using CefSharp.DevTools.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Xml;
+using CefSharp;
+using CefSharp.DevTools.CSS;
+using CefSharp.DevTools.Accessibility;
+using System.Windows.Threading;
+using System.Windows.Media.Animation;
+using System.IO.Ports;
+using System.ComponentModel;
+using LiveCharts;
+using LiveCharts.Defaults;
+using System.Globalization;
+using LiveCharts.Wpf.Charts.Base;
+using LiveCharts.Wpf;
+
+namespace WokwiToComPort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.StateChanged += MainWindow_StateChanged;
+
+            dataGetTimer = new DispatcherTimer();
+            dataGetTimer.Interval = TimeSpan.FromMilliseconds(100);
+            dataGetTimer.Tick += DataGetTimer_Tick;
+
+            cleanMonitorTimer = new DispatcherTimer();
+            cleanMonitorTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            cleanMonitorTimer.Tick += CleanMonitorTimer_Tick;
+
+            try
+            {
+                OpenSerialPort(SerialPort.GetPortNames()[0], 9600);
+                serialPort.Open();
+            }
+            catch (Exception) { }
+
+
+            string[] serialPortNames = SerialPort.GetPortNames();
+            foreach (var item in serialPortNames)
+                ComPortChoose.Items.Add(item);
+            ComPortChoose.SelectedIndex = 0;
+
+
+            grafPointsList.AddRange(Enumerable.Repeat(0, 100));
+        }
+
+
+
+        private SerialPort serialPort;
+
+        private List<int> grafPointsList = new List<int>();
+
+        private DispatcherTimer dataGetTimer;
+        private DispatcherTimer cleanMonitorTimer;
+
+
+        private async void AddDataGraf(int value)
+        {
+            grafPointsList.Add(value);
+            grafPointsList.RemoveAt(0);
+
+            ChartValues<ObservablePoint> grafPoints = new ChartValues<ObservablePoint>();
+
+            for (int i = 0; i < grafPointsList.Count; i++)
+                grafPoints.Add(new ObservablePoint(i + 1, grafPointsList[i]));
+
+            DataContext = new { grafPoints };
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                MainBorder.Margin = new Thickness(6, 6, 6, 36);
+                MainBorder.CornerRadius = new CornerRadius(0);
+            }
+            else
+            {
+                MainBorder.Margin = new Thickness(0, 0, 0, 0);
+                MainBorder.CornerRadius = (CornerRadius)FindResource("BorderCorner");
+            }
+        }
+
+        private void WindowControlHighlighte(object sender, MouseEventArgs e)
+        {
+            Brush defaultColor = (SolidColorBrush)Application.Current.Resources["MainButtonColor"];
+            Brush exitButton = (SolidColorBrush)Application.Current.Resources["HighlightedExitButton"];
+            Brush othersButton = (SolidColorBrush)Application.Current.Resources["HighlightedOthersButton"];
+
+
+            Rectangle currentRectangle;
+
+            switch (((Button)sender).Name)
+            {
+                case "CollapseButton":
+                    currentRectangle = (Rectangle)FindName("CollapseButtonLabel");
+                    currentRectangle.Fill = currentRectangle.Fill == defaultColor ?
+                            othersButton : defaultColor;
+                    break;
+                case "ExpandButton":
+                    currentRectangle = (Rectangle)FindName("ExpandButtonLabel");
+                    currentRectangle.Stroke = currentRectangle.Stroke == defaultColor ?
+                            othersButton : defaultColor;
+                    break;
+                case "ExitButton":
+                    ExitButtonLabel.Fill = ExitButtonLabel.Fill == defaultColor ?
+                            exitButton : defaultColor;
+                    break;
+                default:
+                    throw new Exception("WindowControlsClick run to default");
+            }
+        }
+
+        private void WindowControlClick(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "CollapseButton":
+                    this.WindowState = WindowState.Minimized;
+                    break;
+                case "ExpandButton":
+                    this.WindowState = this.WindowState == WindowState.Maximized ?
+                        WindowState.Normal : WindowState.Maximized;
+                    break;
+                case "ExitButton":
+                    this.Close();
+                    break;
+                default:
+                    throw new Exception("WindowControlsClick run to default");
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+
+        private HtmlDocument GetSiteCode()
+        {
+            string html = "null";
+
+            abb.GetSourceAsync().ContinueWith(taskHtml =>
+            {
+                if (!taskHtml.IsFaulted)
+                    html = taskHtml.Result;
+            }).Wait();
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            return document;
+        }
+
+        private string GetMonitorText()
+        {
+            HtmlDocument document = GetSiteCode();
+
+            HtmlNode preNode = document.DocumentNode.SelectSingleNode("//pre[@class='notranslate']");
+
+            string value = "null";
+
+            if (preNode != null)
+                value = preNode.InnerHtml;
+
+            return value;
+        }
+
+        private async void CleanMonitor()
+        {
+            string script = @"var buttons = document.querySelectorAll('.serial-monitor_serialControls__KSZx8 button'); 
+                            if (buttons.length >= 3) buttons[2].click();";
+
+            await abb.EvaluateScriptAsync(script);
+        }
+
+        private void AnimateMenuGrid(double from, double to, double time)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = from;
+            animation.To = to;
+            animation.Duration = TimeSpan.FromSeconds(time);
+
+
+            TranslateTransform translateTransform = new TranslateTransform();
+            InfoMenu.RenderTransform = translateTransform;
+
+            // Встановлюємо анімацію для властивості Y TranslateTransform
+            translateTransform.BeginAnimation(TranslateTransform.YProperty, animation);
+        }
+
+        bool isActiveMenu = false;
+
+        private void DataGetTimer_Tick(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                int a = 1 + 4;
+            }
+
+            try
+            {
+                string[] values = GetMonitorText().Split('\n');
+
+                //SerialPort serialPort = new SerialPort();
+                string value = values[values.Length - 2];
+                mainButton.Content = value;
+                serialPort.WriteLine(value);
+            }
+            catch (Exception exception)
+            {
+                //MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void CleanMonitorTimer_Tick(object sender, EventArgs e)
+        {
+            CleanMonitor();
+
+            try
+            {
+                AddDataGraf((int)double.Parse(mainButton.Content.ToString().Split()[0], CultureInfo.InvariantCulture));
+            }
+            catch (Exception) { }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGetTimer.Start();
+            cleanMonitorTimer.Start();
+        }
+
+        private void OpenSerialPort(string serialPortName, int baudRate)
+        {
+            serialPort = new SerialPort(serialPortName, baudRate);
+            serialPort.Open();
+        }
+
+        private void ComPortChoose_DropDownOpened(object sender, EventArgs e)
+        {
+            ComPortChoose.Items.Clear();
+
+            string[] serialPortNames = SerialPort.GetPortNames();
+
+            foreach (var item in serialPortNames)
+                ComPortChoose.Items.Add(item);
+        }
+
+        private void ComPortChoose_DropDownClosed(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Close();
+
+            try
+            {
+                OpenSerialPort(ComPortChoose.SelectedItem.ToString(), int.Parse(BaudrateChange.Text));
+            }
+            catch (Exception) { }
+
+
+            //datat.Values = new ChartValues<double> { 1, 2, 3, 4, 5 };
+        }
+
+        private void BaudrateChange_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Close();
+
+            try
+            {
+                OpenSerialPort(ComPortChoose.SelectedItem.ToString(), int.Parse(BaudrateChange.Text));
+            }
+            catch (Exception) { }
+        }
+
+        private void TurnGraf_Click(object sender, RoutedEventArgs e)
+        {
+            if (TestBorder.Height == 0)
+            {
+                TestBorder.Height = 260;
+                ((Button)sender).Content = "Вимкнути графік";
+            }
+            else
+            {
+                TestBorder.Height = 0;
+                ((Button)sender).Content = "Ввімкнути графік";
+            }
+        }
+    }
+}
